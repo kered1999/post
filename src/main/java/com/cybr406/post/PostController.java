0daeee24156f31d0d85f150457a65e6c6bf89175
kered1999 <36059042+kered1999@users.noreply.github.com>
2019-01-21 14:52:26 -0600

    Initial commit

---
+++ src/main/java/com/cybr406/post/PostController.java
@@ -0,0 +1,28 @@
+package com.cybr406.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> createPost(@Valid @RequestBody Post post){
+        return new ResponseEntity<>(postRepository.save(post), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable){
+        return postRepository.findAll(pageable);
+    }
+
+}
---
+++ src/main/java/com/cybr406/post/PostValidator.java
@@ -0,0 +1,23 @@
+package com.cybr406.post;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class PostValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.equals(Post.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(
+                errors, "id", "field.required", "Id is a required field.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(
+                errors, "author", "field.required", "Author is a required field.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(
+                errors, "content", "field.required", "Content is a required field.");
+    }
+}
