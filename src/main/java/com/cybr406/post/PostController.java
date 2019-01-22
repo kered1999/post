85fb7b57468ecb5bee95ff1abc0c73cb4fb38247
steuhm <kered1999@live.com>
2019-01-22 15:05:36 -0600

    all tests good

--- src/main/java/com/cybr406/post/PostController.java
+++ src/main/java/com/cybr406/post/PostController.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 public class PostController {
@@ -20,14 +22,23 @@ public class PostController {
         return new ResponseEntity<Post>(postRepository.save(post), HttpStatus.CREATED);
     }
 
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setValidator(new PostValidator());
+    }
+
     @GetMapping("/posts")
     public Page<Post> getPosts( Pageable pageable){
         return postRepository.findAll(pageable);
     }
 
     @GetMapping("/posts/{id}")
-    public Page<Post> getSinglePosts(@PathVariable(value = "id", required = false) long id, Pageable pageable){
+    public ResponseEntity<Post> getSinglePosts(@PathVariable Long id){
 
-        return postRepository.findAll(pageable);
+        Optional<Post> result = postRepository.findById(id);
+        if(result.isPresent()){
+            return new ResponseEntity<Post>(result.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
